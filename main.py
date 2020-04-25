af48f57fd88b024c501754a742088b512aeb20e9
borksson <64322148+borksson@users.noreply.github.com>
2020-04-25 15:59:59 -0600

    Edited Main File

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import cv2
 from ImgResize import *
 from ShowImage import *
 #Define image path
-inImage = "_assets/dot-to-dot-shapes-39.png" #Path
-# Define showImage
-
+inImage = "_assets/black_circles.png"
 # Load in color image in grayscale
 img = cv2.imread(inImage,0)
+img = imgResize(img, 20)
+showImage(img)
 # Creates an images with thresed values
 ret,thresh=cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 edges=cv2.Canny(thresh,100,200)
@@ -20,15 +20,23 @@ edges=cv2.Canny(thresh,100,200)
 showImage(edges)
 # Identify and outline circles
 cimg = cv2.cvtColor(edges,cv2.COLOR_GRAY2BGR)
-circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-                            param1=50,param2=30,minRadius=10,maxRadius=300)
-print(circles)
-if circles.any():
+minDist = 20
+param1 = 100
+param2 = 20s
+minRadius = 20
+maxRadius = 30
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,minDist,
+                            param1=param1,param2=param2,minRadius=minRadius,maxRadius=maxRadius)
+if circles is not None:
+    print(circles, len(circles[0]) )
     for i in circles[0,:]:
         # draw the outer circle
         if(i[2]>=0):
             cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),1)
         # draw the center of the circle
             cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+    showImage(cimg)
+else:
+    print("No circles")
+    minDist-=1
 # Show image
-showImage(cimg)
