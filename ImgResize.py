3bd1e5ae21e49c8a3c42103512a18577f5ca5181
borksson <64322148+borksson@users.noreply.github.com>
2020-04-25 14:48:26 -0600

    Cleaned up ImgResize

--- ImgResize.py
+++ ImgResize.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # importing
@@ -13,13 +12,10 @@ def imgResize(src, scale_percent):
     width = int(src.shape[1] * scale_percent / 100)
     height = int(src.shape[0] * scale_percent / 100)
     dim = (width, height)
-    return cv2.resize(img, dim)
+    return cv2.resize(src, dim)
 # defines image path
 imgPath = "_assets/black_circles.png"
 # Load an color image in grayscale
 src = cv2.imread(imgPath,0)
-myTuple = (255,330)
-# resizes image
-img = cv2.resize(src, myTuple)
-# showImage
+img = imgResize(src, 20)
 showImage(img)
