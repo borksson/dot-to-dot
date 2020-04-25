5caa1d4342b47e7d64560a412ae266b52a124e69
borksson <64322148+borksson@users.noreply.github.com>
2020-04-25 14:58:20 -0600

    Update ImgResize.py

--- ImgResize.py
+++ ImgResize.py
@@ -1,7 +1,8 @@
 # coding: utf-8
+from cv2 import resize
 #Defines Image Resize
 def imgResize(src, scale_percent):
     width = int(src.shape[1] * scale_percent / 100)
     height = int(src.shape[0] * scale_percent / 100)
     dim = (width, height)
-    return cv2.resize(src, dim)
+    return resize(src, dim)
